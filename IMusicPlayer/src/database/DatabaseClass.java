561c7e12a79a1cd62ba6abb81ecabdc947293112
manuel <manuel@manuel-PC>
2015-11-12 18:14:11 +0100

    Pusht

--- IMusicPlayer/src/database/DatabaseClass.java
+++ IMusicPlayer/src/database/DatabaseClass.java
@@ -7,7 +7,7 @@ public class DatabaseClass {
 	SQLiteDatabase newDatabase;
 	
 	public void CreateDatabase() {
-		newDatabase = openOrCreateDatabase("Meine Datenbank", MODE_PRIVATE, null);
+		//newDatabase = openOrCreateDatabase("Meine Datenbank", MODE_PRIVATE, null);
 		newDatabase.execSQL("CREATE TABLE IF NOT EXISTS Text (texteingabedb VARCHAR);");
 	}
 
