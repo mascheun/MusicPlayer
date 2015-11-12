38d0e413ea6b4f697b49d9adb37182f5fd5fe3ad
felix <f@f-HP>
2015-11-12 17:38:42 +0100

    Implemented a popup menu for future development

--- IMusicPlayer/src/com/example/imusicplayer/MainActivity.java
+++ IMusicPlayer/src/com/example/imusicplayer/MainActivity.java
@@ -7,24 +7,34 @@ import com.example.imusicplayer.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 import functions.SongsManager;
 
 public class MainActivity extends Activity {
 	
-	private Button button1;
-	private Button button2;
-	private TextView tv1;
 	private SongsManager sm = new SongsManager();
 	private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+	private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mTitle;
+ 
+    private String[] drawerTitles;
+    private String[] drawerSubtitles;
+    private int[] drawerIcons;
 
 
 	@Override
@@ -32,57 +42,108 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		addListenerOnButton();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	
-	
-	public void addListenerOnButton() {
-
-		button1 = (Button) findViewById(R.id.button1);
-
-		button1.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				songsList = sm.getPlayList();
-				sm.playSong(1, songsList);
-//				button2 = (Button) findViewById(R.id.button2);
-//				button2.setVisibility(Button.VISIBLE);
-
-			}
-
-		});
+		 mTitle = getTitle();
+		 
+	        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+	        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+	        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+	 
+	        // Hole die Titel aus einem Array aus der strings.xml
+	        drawerTitles = getResources().getStringArray(R.array.drawerTitles_array);
+	        // Setzt die Icons zu den Einträgen
+	        drawerIcons = new int[] {android.R.drawable.ic_menu_info_details, android.R.drawable.ic_menu_edit, android.R.drawable.ic_menu_delete};
+	 
+	        // Erstellt den neuen MenuAdapter aus der Klasse MenuListAdapter
+	        MenuListAdapter mMenuAdapter = new MenuListAdapter(this, drawerTitles, drawerSubtitles, drawerIcons);
+	        mDrawerList.setAdapter(mMenuAdapter);
+	        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+	 
+	        // Bereitet die ActionBar auf den Navigation Drawer vor
+	        getActionBar().setDisplayHomeAsUpEnabled(true);
+	        getActionBar().setHomeButtonEnabled(true);
+	 
+	        // Fügt den Navigation Drawer zur ActionBar hinzu
+	        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+	            public void onDrawerClosed(View view) {
+	                getActionBar().setTitle(mTitle);
+	                invalidateOptionsMenu();
+	            }
+	 
+	            public void onDrawerOpened(View drawerView) {
+	                getActionBar().setTitle(R.string.app_name);
+	                invalidateOptionsMenu();
+	            }
+	        };
+	        mDrawerLayout.setDrawerListener(mDrawerToggle);
 		
-//		button2 = (Button) findViewById(R.id.button2);
-//
-//		button2.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View arg0) {
-//				sm.playSong(0, songsList);
-//			}
-//
-//		});
 	}
+
+	 // Fügt das Menü hinzu / ActionBar Einträge
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+ 
+    // Versteckt die ActionBar-Einträge, sobald der Drawer ausgefahren is
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+ 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Öffnet und schließt den Navigation Drawer bei Klick auf den Titel/das Icon in der ActionBar
+        if (item.getItemId() == android.R.id.home) {
+            if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
+                mDrawerLayout.closeDrawer(mDrawerList);
+            } else {
+                mDrawerLayout.openDrawer(mDrawerList);
+            }
+        }
+ 
+        // Gibt den ActionBar-Buttons Funktionen
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+            	Toast.makeText(null, "Aktualisieren gedrückt!", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+ 
+    // Listener für die Navigation Drawer Einträge - Achtung: Zählung beginnt bei 0!
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            if (position == 0) {
+                // Aktion
+            } else if (position == 1) {
+                // Aktion
+            } else if (position == 2) {
+                // Aktion
+            }
+ 
+            mDrawerList.setItemChecked(position, true);
+            mTitle = drawerTitles[position];
+            getActionBar().setTitle(mTitle);
+            mDrawerLayout.closeDrawer(mDrawerList);
+        }
+    }
+ 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+ 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 	
 }
---
+++ IMusicPlayer/src/com/example/imusicplayer/MenuListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.imusicplayer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+ 
+public class MenuListAdapter extends BaseAdapter {
+    private Context context;
+    private String[] mTitle;
+    private String[] mSubTitle;
+    private int[] mIcon;
+    private LayoutInflater inflater;
+ 
+    public MenuListAdapter(Context pContext, String[] pTitle, String[] pSubtitle, int[] pIcon) {
+        context = pContext;
+        mTitle = pTitle;
+        mSubTitle = pSubtitle;
+        mIcon = pIcon;
+    }
+ 
+    public View getView(int position, View convertView, ViewGroup parent) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.drawer_list_item, parent, false);
+ 
+        TextView txtTitle = (TextView) itemView.findViewById(R.id.title);
+        ImageView imgIcon = (ImageView) itemView.findViewById(R.id.icon);
+ 
+        txtTitle.setText(mTitle[position]);
+        imgIcon.setImageResource(mIcon[position]);
+ 
+        return itemView;
+    }
+ 
+    @Override
+    public int getCount() {
+        return mTitle.length;
+    }
+ 
+    @Override
+    public Object getItem(int position) {
+        return mTitle[position];
+    }
+ 
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+}
