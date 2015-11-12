7bf96541f82de7d4990a09cdfa7321c8b995ab0b
manuel <manuel@manuel-PC>
2015-11-12 19:48:15 +0100

    Version doesnt work anymore

--- IMusicPlayer/src/com/example/imusicplayer/MainActivity.java
+++ IMusicPlayer/src/com/example/imusicplayer/MainActivity.java
@@ -6,11 +6,7 @@ import java.util.HashMap;
 import com.example.imusicplayer.R;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
@@ -21,10 +17,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+import database.DatabaseClass;
 import functions.SongsManager;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends Activity {
 	
 	private SongsManager sm = new SongsManager();
@@ -36,61 +36,80 @@ public class MainActivity extends Activity {
     private String[] drawerTitles;
     private String[] drawerSubtitles;
     private int[] drawerIcons;
+    private DatabaseClass db;
+    Button button;
+    Button button1;
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		db = new DatabaseClass();
+		
+		db.CreatePlayListDatabase();
+		
+		
 		setContentView(R.layout.activity_main);
 		
-		 mTitle = getTitle();
+		mTitle = getTitle();
 		 
-	        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-	        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-	        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-	 
-	        // Hole die Titel aus einem Array aus der strings.xml
-	        drawerTitles = getResources().getStringArray(R.array.drawerTitles_array);
-	        // Setzt die Icons zu den Einträgen
-	        drawerIcons = new int[] {android.R.drawable.ic_menu_manage, android.R.drawable.ic_menu_edit, android.R.drawable.ic_menu_delete};
-	 
-	        // Erstellt den neuen MenuAdapter aus der Klasse MenuListAdapter
-	        MenuListAdapter mMenuAdapter = new MenuListAdapter(this, drawerTitles, drawerSubtitles, drawerIcons);
-	        mDrawerList.setAdapter(mMenuAdapter);
-	        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-	 
-	        // Bereitet die ActionBar auf den Navigation Drawer vor
-	        getActionBar().setDisplayHomeAsUpEnabled(true);
-	        getActionBar().setHomeButtonEnabled(true);
-	 
-	        // Fügt den Navigation Drawer zur ActionBar hinzu
-	        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
-	            public void onDrawerClosed(View view) {
-	                getActionBar().setTitle(mTitle);
-	                invalidateOptionsMenu();
-	            }
-	 
-	            public void onDrawerOpened(View drawerView) {
-	                getActionBar().setTitle(R.string.app_name);
-	                invalidateOptionsMenu();
-	            }
-	        };
-	        mDrawerLayout.setDrawerListener(mDrawerToggle);
-	        
-	        final Button button = (Button) findViewById(R.id.button1);
-	         button.setOnClickListener(new View.OnClickListener() {
-	             public void onClick(View v) {
-	                 // Perform action on click
-	             }
-	         });
-	         
-	        final Button button2 = (Button) findViewById(R.id.button2);
-	         button.setOnClickListener(new View.OnClickListener() {
-	             public void onClick(View v) {
-	                 // Perform action on click
-	             }
-	         });
-
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+ 
+        // Hole die Titel aus einem Array aus der strings.xml
+        drawerTitles = getResources().getStringArray(R.array.drawerTitles_array);
+        // Setzt die Icons zu den Einträgen
+        drawerIcons = new int[] {android.R.drawable.ic_menu_manage, android.R.drawable.ic_menu_edit, android.R.drawable.ic_menu_delete};
+ 
+        // Erstellt den neuen MenuAdapter aus der Klasse MenuListAdapter
+        MenuListAdapter mMenuAdapter = new MenuListAdapter(this, drawerTitles, drawerSubtitles, drawerIcons);
+        mDrawerList.setAdapter(mMenuAdapter);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+ 
+        // Bereitet die ActionBar auf den Navigation Drawer vor
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+ 
+        // Fügt den Navigation Drawer zur ActionBar hinzu
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu();
+            }
+ 
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(R.string.app_name);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        
+        final EditText ed = (EditText) findViewById(R.id.editText1);
+        
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	db.addPlayList(ed.getText().toString());
+            }
+        });
+         
+        final TextView tv = (TextView) findViewById(R.id.textView1);
+         
+        button1 = (Button) findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	ArrayList<String> lists = db.showPlayLists();
+            	String s = "";
+            	for(String text : lists) {
+            		s = s + text + "\n";
+            	}
+            	tv.setText(s);
+            }
+        });
 		
 	}
 
