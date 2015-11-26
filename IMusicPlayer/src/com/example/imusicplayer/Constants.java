9b0e2e2a21794fd2d1f35c69655e1a083f0f799d
felix <f@f-HP>
2015-11-26 17:26:27 +0100

    constants class

---
+++ IMusicPlayer/src/com/example/imusicplayer/Constants.java
@@ -0,0 +1,7 @@
+package com.example.imusicplayer;
+
+public abstract class Constants {
+	public static int SONGSFROMPLAYLIST = 2;
+	public static int REGULARSONG = 1;
+	public static String PLAYLISTKEY = "playlist";
+}
